257338244266d992b8a11924fdd0f12b998109ff
Sarah Murphree <104289709+smurph7894@users.noreply.github.com>
2023-01-14 16:23:41 -0800

    Initial commit

---
+++ src/main/firstProgram.java
@@ -0,0 +1,7 @@
+public class firstProgram {
+    public static void main(String[] args) {
+        System.out.println("My Name is Sarah Murphree");
+        System.out.println("I am blank years old");
+        System.out.println("My hometown is in Washington");
+    }
+}
